4cdeb662a574458bab3845d7d61ed26b39fa4567
Spyros Giannakopoulos <spyros.giannakopoulo@sainsburys.co.uk>
2023-03-08 17:23:59 +0000

    Finished layout for Main Activity and created Search Repo Activity

---
+++ app/src/main/java/com/example/findrepo/SearchRepoActivitty.kt
@@ -0,0 +1,11 @@
+package com.example.findrepo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchRepoActivitty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_repo)
+    }
+}
